4db7051606f354cba7fd38571eb0409031cb4927
bomfunksn <xpehbomfunk@gmail.com>
2025-09-09 14:12:33 +0200

    fix

--- Assets/Player_IdleState.cs
+++ Assets/Player_IdleState.cs
@@ -13,8 +13,10 @@ public class Player_IdleState : Player_GroundedState
     public override void Update()
     {
         base.Update();
+        if (player.moveInput.x == player.facingDir && player.wallDetected)
+            return;
 
         if (player.moveInput.x != 0)
-            stateMachine.ChangeState(player.moveState);
+                stateMachine.ChangeState(player.moveState);
     }
 }
