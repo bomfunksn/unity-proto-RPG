c802abcb87c77bcd1b5196c20f28cd3d06489f0c
bomfunksn <xpehbomfunk@gmail.com>
2025-09-10 12:37:35 +0200

    loop respawn BG

--- Assets/Scripts/ParallaxBackground.cs
+++ Assets/Scripts/ParallaxBackground.cs
@@ -4,11 +4,15 @@ public class ParallaxBackground : MonoBehaviour
 {
     private Camera mainCamera;
     private float lastCameraPositionX;
+    private float cameraHalfWidth;
+
     [SerializeField] private ParallaxLayer[] backgroundLayers;
 
     private void Awake()
     {
         mainCamera = Camera.main; //Что такое .main??? это типа название главной камеры движка, которой мы управляем синемашином?
+        cameraHalfWidth = mainCamera.orthographicSize * mainCamera.aspect; //Как из этого получилась половина ширины? Что такое "рахмер камеры" в ортографик сайзе?
+        CalculateImageLenghth();
     }
 
     private void Update()
@@ -17,9 +21,18 @@ public class ParallaxBackground : MonoBehaviour
         float distanceToMove = currentCameraPositionX - lastCameraPositionX;
         lastCameraPositionX = currentCameraPositionX;
 
+        float cameraLeftEdge = currentCameraPositionX - cameraHalfWidth;
+        float cameraRightEdge = currentCameraPositionX + cameraHalfWidth;
+
         foreach (ParallaxLayer layer in backgroundLayers)
         {
             layer.Move(distanceToMove);
+            layer.LoopBackround(cameraLeftEdge, cameraRightEdge);//зачем тут писать в скобках при вызове метода?
         }
+    }
+    private void CalculateImageLenghth()
+    {
+        foreach (ParallaxLayer layer in backgroundLayers)
+            layer.CalculateImageWidth();
     }
 }
--- Assets/Scripts/ParallaxLayer.cs
+++ Assets/Scripts/ParallaxLayer.cs
@@ -5,9 +5,30 @@ public class ParallaxLayer
     [SerializeField] private Transform background;
     [SerializeField] private float ParallaxMultiplier;
 
+    private float imageFullWidth;
+    private float imageHalfWidth;
+
+    public void CalculateImageWidth()
+    {
+        imageFullWidth = background.GetComponent<SpriteRenderer>().bounds.size.x;
+        imageHalfWidth = imageFullWidth / 2;
+    }
+
     public void Move(float distanceToMove)
     {
         background.position += Vector3.right * (distanceToMove * ParallaxMultiplier);
     }
+
+    public void LoopBackround(float cameraLeftEdge, float cameraRightEdge) //зачем указывать значения в скобках?
+    {
+        float imageRightEdge = background.position.x + imageHalfWidth;
+        float imageLeftEdge = background.position.x - imageHalfWidth;
+
+        if (imageRightEdge < cameraLeftEdge)
+            background.position += Vector3.right * imageFullWidth;
+        else if (imageLeftEdge > cameraRightEdge)
+            background.position += Vector3.right * -imageFullWidth;
+    }
+
 
 }
