d0779f95e20dcd3a91be9deb8dbb0a3c8763971b
Xpeh <xpehbomfunk@gmail.com>
2025-09-30 23:30:12 +0200

    can dash during wallslide

--- Assets/Scripts/Player/PlayerStates/Player_WallSlideState.cs
+++ Assets/Scripts/Player/PlayerStates/Player_WallSlideState.cs
@@ -10,7 +10,7 @@ public class Player_WallSlideState : PlayerState
     public override void Update()
     {
         base.Update();
-        HandleWallSlide(); //ПОЧЕМУ ОНО НЕ ВЫЗЫВАЕТСЯ ДО ПРИЛИПАНИЯ К СТЕНЕ
+        HandleWallSlide();
 
 
         if (input.Player.Jump.WasPressedThisFrame())
@@ -20,19 +20,24 @@ public class Player_WallSlideState : PlayerState
             stateMachine.ChangeState(player.fallState);
 
         if (player.groundDetected)
-            {
-                stateMachine.ChangeState(player.idleState);
+        {
+            stateMachine.ChangeState(player.idleState);
 
-                if(player.facingDir != player.moveInput.x)
+            if (player.facingDir != player.moveInput.x)
                 player.Flip();
-            }
+        }
+        if (input.Player.Dash.WasPressedThisFrame() && player.wallDetected)
+        {
+            player.Flip();
+            stateMachine.ChangeState(player.dashState);
+        }    
     }
 
     private void HandleWallSlide()
     {
-        if (player.moveInput.y < 0) //ВОТ ТУТ, если я жму вниз, я лечу вниз, с той же скоростью, всё ок
+        if (player.moveInput.y < 0) 
             player.SetVelocity(player.moveInput.x, rb.linearVelocity.y);
         else
-            player.SetVelocity(player.moveInput.x, rb.linearVelocity.y * player.wallSlideSlowMultiplier); // НО если я не жму вниз, например, при падении, почему я не замедляюсь. Тут же наследование от ентити напрямую.
+            player.SetVelocity(player.moveInput.x, rb.linearVelocity.y * player.wallSlideSlowMultiplier); 
     }
 }
