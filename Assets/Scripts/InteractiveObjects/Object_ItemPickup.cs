9fc59a86c78e3c4fad3102308201f3f5f275732f
Xpeh <xpehbomfunk@gmail.com>
2025-10-28 20:40:55 +0100

    basic inventory

--- Assets/Scripts/InteractiveObjects/Object_ItemPickup.cs
+++ Assets/Scripts/InteractiveObjects/Object_ItemPickup.cs
@@ -6,11 +6,19 @@ public class Object_ItemPickup : MonoBehaviour
 
     [SerializeField] private ItemDataSO itemData;
 
+    private Inventory_Item itemToAdd;
+    private Inventory_Base inventory;
+
+    private void Awake()
+    {
+        itemToAdd = new Inventory_Item(itemData);
+    }
+
     private void OnValidate()
     {
         if (itemData == null)
             return;
-            
+
         sr = GetComponent<SpriteRenderer>();
         sr.sprite = itemData.itemIcon;
         gameObject.name = "Object_ItemPickup - " + itemData.itemName;
@@ -18,8 +26,13 @@ public class Object_ItemPickup : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("Player picked up item - " + itemData.itemName);
-        Destroy(gameObject);
+        inventory = collision.GetComponent<Inventory_Base>();
+
+        if(inventory != null && inventory.CanAddItem())
+        {
+            inventory.AddItem(itemToAdd);
+            Destroy(gameObject);
+        }
     }
 
 }
